48257d2bbabe0c07522d4eeefb8a59ed5bb099cd
vj703676 <jules.vesnat@etu.unice.fr>
2022-12-03 18:19:34 +0100

    ajout système authentification LocalStorage Token 2

--- packages/Client/src/App.js
+++ packages/Client/src/App.js
@@ -3,12 +3,9 @@ import './App.css';
 import AdminPixelBoard from "./components/AdminPixelBoard/AdminPixelBoard";
 import {BrowserRouter, HashRouter, Route, Switch} from "react-router-dom";
 import PixelBoard from "./components/PixelBoard/PixelBoard";
-<<<<<<< HEAD
 import Login from './components/Login/Login';
 import useToken from './useToken';
-=======
 import Navbar from './components/Navbar/Navbar';
->>>>>>> 9e01285465f934f034d3b61710206e0ec1b9aa6e
 
 function setToken(userToken) {
     sessionStorage.setItem('token', JSON.stringify(userToken));

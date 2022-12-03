2f51012b10b11d55c74040554c0f79ab192c15b0
Oussama SAMIA <oussamasamia1@gmail.com>
2022-12-03 23:45:12 +0100

    Correct getByPosition Pixel @Server

--- packages/Server/controllers/pixelController.js
+++ packages/Server/controllers/pixelController.js
@@ -1,40 +1,43 @@
 const PixelService = require("../services/PixelService");
 const PixelBoardService = require("../services/pixelBoardService");
-const { ObjectId } = require("mongodb");
+const {ObjectId} = require("mongodb");
 
 exports.getByPosition = async (req, res) => {
 
-  //http://localhost:8080/api/pixels/getByPosition?pbId=638b8c7a7de9dcf5eba01130&posY=4&posX=3
+    //http://localhost:8080/api/pixels/getByPosition?pbId=638b8c7a7de9dcf5eba01130&posY=4&posX=3
 
 
-  const pb = await PixelBoardService.getPixelBoardById(req.query.pbId);
+    const pb = await PixelBoardService.getPixelBoardById(req.query.pbId);
+
+    try {
+        let isFound = false;
+
+        let pixelId;
+
+        pb.pixels.forEach((element) => {
+
+            if (element.positionX == req.params.posX && element.positionY == req.params.posY) {
+
+                pixelId = element._id;
+                // res.json({data: element._id, status: "success"});
+                isFound = true;
+            }
+
+            if (isFound == false) {
+                res.status(404).send('pixel with X =' + req.params.posX + 'and Y = ' + req.params.posY + ' Not found');
+            }
 
 
-try{
-let isFound= false ;
-  pb.pixels.forEach(  (element) => { 
-
-if ( element.positionX == req.params.posX  && element.positionY == req.params.posY  ){
-
-  res.json({ data: element._id, status: "success" });
-isFound = true;
+        });
 
 
-}
-if (isFound == false){
-  res.status(404).send('pixel with X ='+ req.params.posX + 'and Y = ' +req.params.posY +' Not found');
-}
+        res.json({dataa: pixelId, status: "success"});
 
 
+    } catch {
+        res.status(404).send('pixelBoard with id =' + req.params.pbId + ' Not found');
 
-
-  });
-    
-
-}catch{
-  res.status(404).send('pixelBoard with id ='+ req.params.pbId +' Not found');
-
-}
+    }
 
 };
 
@@ -42,50 +45,48 @@ exports.getAllPixels = async (req, res) => {
     try {
         const pixels = await PixelService.getAllPixels();
         console.log(pixels);
-        res.json({ data: pixels, status: "success" });
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
+        res.json({data: pixels, status: "success"});
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
 };
-
-
 
 
 exports.createPixel = async (req, res) => {
     try {
-      const pixel = await PixelService.createPixel(req.body);
-      res.json({ data: pixel, status: "success" });
+        const pixel = await PixelService.createPixel(req.body);
+        res.json({data: pixel, status: "success"});
     } catch (err) {
-      res.status(500).json({ error: err.message });
+        res.status(500).json({error: err.message});
     }
-  };
+};
 
-  exports.getPixelById = async (req, res) => {
+exports.getPixelById = async (req, res) => {
     try {
 
-        
-      const pixel = await PixelService.getPixelById(req.params.id);
 
-      res.json({ data: pixel, status: "success" });
+        const pixel = await PixelService.getPixelById(req.params.id);
+
+        res.json({data: pixel, status: "success"});
     } catch (err) {
-      res.status(500).json({ error: err.message });
+        res.status(500).json({error: err.message});
     }
-  };
-   
-  exports.updatePixel = async (req, res) => {
+};
+
+exports.updatePixel = async (req, res) => {
     try {
-      const pixel = await PixelService.updatePixel(req.params.id, req.body);
-      res.json({ data: pixel.id, status: "success" });
+        const pixel = await PixelService.updatePixel(req.params.id, req.body);
+        res.json({data: pixel.id, status: "success"});
     } catch (err) {
-      res.status(500).json({ error: err.message });
+        res.status(500).json({error: err.message});
     }
-  };
-   
-  exports.deletePixel = async (req, res) => {
+};
+
+exports.deletePixel = async (req, res) => {
     try {
-      const pixel = await PixelService.deletePixel(req.params.Pseudo);
-      res.json({ data: pixel, status: "success" });
+        const pixel = await PixelService.deletePixel(req.params.Pseudo);
+        res.json({data: pixel, status: "success"});
     } catch (err) {
-      res.status(500).json({ error: err.message });
+        res.status(500).json({error: err.message});
     }
-  };
+};

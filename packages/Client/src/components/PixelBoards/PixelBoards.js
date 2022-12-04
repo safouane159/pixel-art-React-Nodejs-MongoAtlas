3150e8987dd9176e3f2c39adcbc14d8c88968062
Oussama SAMIA <oussamasamia1@gmail.com>
2022-12-04 15:40:36 +0100

    Correct getPixelBoardById @Server

--- packages/Client/src/components/PixelBoards/PixelBoards.js
+++ packages/Client/src/components/PixelBoards/PixelBoards.js
@@ -4,15 +4,34 @@ import PixelCard from "../PixelCards/PixelCard";
 
 
 function PixelBoards() {
+    const server_url = "http://localhost:8080";
+
+
+    const [pixelBoards, setPixelBoards] = useState([<PixelCard/>, <PixelCard/>,]);
+
+    useEffect(() => {
+
+        getListOfPB();
+
+    }, []);
+
+    async function getListOfPB() {
+
+
+        await fetch(server_url + "/api/pixelBoard", {
+            method: "GET",
+        }).then(response => response.json())
+            .then(response => {
+                console.log(response)
+            });
+    }
+
+
     return (
         <div className={"pixelBoards"}>
-            <PixelCard/>
-            <PixelCard/>
-            <PixelCard/>
-            <PixelCard/>
-            <PixelCard/>
-            <PixelCard/>
-            <PixelCard/>
+            {
+                pixelBoards
+            }
         </div>
     )
 }
--- packages/Server/controllers/pixelController.js
+++ packages/Server/controllers/pixelController.js
@@ -4,40 +4,47 @@ const {ObjectId} = require("mongodb");
 
 exports.getByPosition = async (req, res) => {
 
-  //http://localhost:8080/api/pixels/getByPosition?pbId=638b8c7a7de9dcf5eba01130&posY=4&posX=3
+    //http://localhost:8080/api/pixels/getByPosition?pbId=638b8c7a7de9dcf5eba01130&posY=4&posX=3
 
 
     const pb = await PixelBoardService.getPixelBoardById(req.query.pbId);
 
+    //console.log(pb);
     try {
-        let isFound = false;
 
         let pixelId;
 
-        pb.pixels.forEach((element) => {
+        pb.pixels.forEach(async (element, index) => {
 
-            if (element.positionX == req.params.posX && element.positionY == req.params.posY) {
+            const pixel = await PixelService.getPixelById(element);
+            //console.log(pixel);
 
-                pixelId = element._id;
-                // res.json({data: element._id, status: "success"});
-                isFound = true;
-            }
+            /*console.log(pixel._id)
+            console.log(req.query.posX)
+            console.log("X"+pixel.positionX)
+            console.log(req.query.posY)
+            console.log("Y"+pixel.positionY)*/
 
-            if (isFound == false) {
-                res.status(404).send('pixel with X =' + req.params.posX + 'and Y = ' + req.params.posY + ' Not found');
+
+            if (pixel.positionX == req.query.posX && pixel.positionY == req.query.posY) {
+
+                console.log("test");
+                pixelId = pixel._id;
+                //console.log(element);
+                res.json({data: pixelId, status: "success"});
+
             }
 
 
         });
 
+        //res.json({data: pixelId, status: "success"});
 
-        res.json({data: pixelId, status: "success"});
-    
 
     } catch {
-        res.status(404).send('pixelBoard with id =' + req.params.pbId + ' Not found');
+        res.status(404).send('NOT FOUND');
 
-}
+    }
 
 };
 
@@ -46,47 +53,47 @@ exports.getAllPixels = async (req, res) => {
         const pixels = await PixelService.getAllPixels();
         console.log(pixels);
         res.json({data: pixels, status: "success"});
-      } catch (err) {
+    } catch (err) {
         res.status(500).json({error: err.message});
-      }
+    }
 };
 
 
 exports.createPixel = async (req, res) => {
     try {
-      const pixel = await PixelService.createPixel(req.body);
+        const pixel = await PixelService.createPixel(req.body);
         res.json({data: pixel, status: "success"});
     } catch (err) {
         res.status(500).json({error: err.message});
     }
-  };
+};
 
-  exports.getPixelById = async (req, res) => {
+exports.getPixelById = async (req, res) => {
     try {
 
-        
-      const pixel = await PixelService.getPixelById(req.params.id);
+
+        const pixel = await PixelService.getPixelById(req.params.id);
 
         res.json({data: pixel, status: "success"});
     } catch (err) {
         res.status(500).json({error: err.message});
     }
-  };
-   
-  exports.updatePixel = async (req, res) => {
+};
+
+exports.updatePixel = async (req, res) => {
     try {
-      const pixel = await PixelService.updatePixel(req.params.id, req.body);
+        const pixel = await PixelService.updatePixel(req.params.id, req.body);
         res.json({data: pixel.id, status: "success"});
     } catch (err) {
         res.status(500).json({error: err.message});
     }
-  };
-   
-  exports.deletePixel = async (req, res) => {
+};
+
+exports.deletePixel = async (req, res) => {
     try {
-      const pixel = await PixelService.deletePixel(req.params.Pseudo);
+        const pixel = await PixelService.deletePixel(req.params.Pseudo);
         res.json({data: pixel, status: "success"});
     } catch (err) {
         res.status(500).json({error: err.message});
     }
-  };
+};
